97a9bd391d791fa29d5022cfd4fe86db47e3d478
Lu Zhang <luz20@pitt.edu>
2019-08-07 18:02:38 +0800

    lift states to app.js

--- src/App.js
+++ src/App.js
@@ -12,9 +12,50 @@ class App extends Component {
         super(props);
 
         this.state = {
-            applicationName: 'Traffic Map'
+            applicationName: 'Traffic Map',
+            data: null,
+            api_url: 'https://data.edmonton.ca/resource/87ck-293k.json'
         }
     };
+
+    createFeatureCollection(data) {
+        let features = [];
+        data.forEach(point => {
+            features.push({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        parseFloat(point.location.longitude),
+                        parseFloat(point.location.latitude)
+                    ]
+                },
+                "properties": {
+                    "name": point.disruption_number,
+                    "description": point.description,
+                    "details": point.details,
+                    "duration": point.duration,
+                    "impact": point.impact
+                }
+            });
+        });
+
+        return {
+            "type": "FeatureCollection",
+            "features": features
+        };
+    }
+
+    componentDidMount() {
+        const { data, api_url } = this.state;
+
+        if (!data) {
+            fetch(api_url, { method: 'GET' })
+                .then(response => response.json())
+                .then(response => this.createFeatureCollection(response))
+                .then(response => this.setState({ data: response }));
+        }
+    }
 
     render() {
         return (
@@ -22,7 +63,7 @@ class App extends Component {
             <Header appName={this.state.applicationName}/>
 
             <Container>
-                <Map />
+                <Map data={this.state.data}/>
             </Container>
             </div>
         );
